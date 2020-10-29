aef1940e7ae068bcd3a7545d178074a72e5a57d4
ixmar <jack.silver.k@gmail.com>
2020-10-29 19:35:11 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!usr/bin/env bash
+
+#get the dir of the current script
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+
+#vim
+if [[ ! -a ~/.vimrc ]]; then
+   ln -s $script_dir/vim/vimrc ~/.vimrc
+fi
