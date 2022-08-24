a120971f1af60eb2dc6a71d2823cf17c25e5d45c
jason <jason@email.com>
2022-08-24 15:23:39 +0800

    Switch YCM to  CoC

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [[ ! -a ~/.vimrc ]]; then
    ln -s $script_dir/vim/vimrc ~/.vimrc
 fi
+
+#coc
+if [[ ! -a ~/.vim/coc-settings.json ]]; then
+   mkdir -p ~/.vim && ln -sf $script_dir/vim/coc.nvim/coc-settings.json ~/.vim/coc-settings.json
+fi
